c96f41b6f20dd2f2361a3d99eab94fe4a1e6476d
Manh Cuong <cuongpigerr@gmail.com>
2021-11-07 22:38:04 +0700

    .

---
+++ Chapter 04. Getting Started with Layouts and Material Design/source_myapp/app/src/main/java/com/manhcuong/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.manhcuong.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+
+    public void loadConstraintLayout(View v) {
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loadTableLayout(View v) {
+//        setContentView(R.layout.my_table_layout);
+    }
+}
