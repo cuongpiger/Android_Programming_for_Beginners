0a4b701b787592b905f4a8dc3c87930cb930a771
Manh Cuong <cuongpigerr@gmail.com>
2021-11-08 00:47:10 +0700

    .

--- Chapter 04. Getting Started with Layouts and Material Design/source_myapp/app/src/main/java/com/manhcuong/myapplication/MainActivity.java
+++ Chapter 04. Getting Started with Layouts and Material Design/source_myapp/app/src/main/java/com/manhcuong/myapplication/MainActivity.java
@@ -20,4 +20,8 @@ public class MainActivity extends AppCompatActivity {
     public void loadTableLayout(View v) {
 //        setContentView(R.layout.my_table_layout);
     }
+
+    public void loadMenuLayout(View v) {
+        setContentView(R.layout.main_menu);
+    }
 }
