bd08fcbd7db111c158150e1c0f241cef624e9fdf
Manh Cuong <cuongpigerr@gmail.com>
2021-11-09 09:12:00 +0700

    .

---
+++ Chapter 10. Object-Oriented Programming/app/src/main/java/com/manhcuong/myapplication/Soldier.java
@@ -0,0 +1,12 @@
+package com.manhcuong.myapplication;
+
+import android.util.Log;
+
+public class Soldier {
+    int health;
+    String soldierType;
+
+    void shootEnemy() {
+        Log.i(soldierType, " is shooting");
+    }
+}
